ae971daee74c850197fcbd70f6f13e41fdd78a40
megha prasad <meghaprasadkeecheril@gmail.com>
2022-11-30 22:01:10 +0530

    menu page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from '../sevices/order-details.service';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:OrderDetailsService) { }
 
+  foodData:any
   ngOnInit(): void {
+    this.foodData=this.service.foodDetails
   }
 
 }
